60446872ffa8f6420e5698b643be8d7666d6fc35
azinazarkar <azin.azarkar@gmail.com>
2020-12-06 18:42:03 +0330

    readme added

--- venv/main_sniffer.py
+++
@@ -1,8 +0,0 @@
-import socket,sys,struct
-
-# create a network socket using the default constructor
-try:
-  sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-except socket.error:
-  print('Socket could not be created.')
-  sys.exit(1)
